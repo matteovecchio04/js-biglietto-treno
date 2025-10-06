2b90ae574b3e70e909e624f17b66cfc03bc4ea46
matteovecchio04 <matteovex@gmail.com>
2025-10-06 15:42:44 +0200

    Human code and tools that I think will be useful to complete the various tasks

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+/*
+
+Il programma chiederà all'utente il numero di kilometri che vorrà percorrere e l'età dell'utente
+
+Calcolare il prezzo totale del viaggio, secondo queste regole:
+- Il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- Va applicato uno sconto del 20% per i minorenni
+- Va applicato uno sconto del 40% per gli over 65.
+- L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+*/
+
+/* TOOLS:
+
+-variables: let/const for the ages (<18 and >65)
+-alert: to have a visual representation of the code
+-console.log: to test the code in the console
+-If age <18 true --> apply 20% discount
+-Else If age >65 true --> apply 40% disocunt
+-Else apply 0% discount
+-number of km?
+-const: 0.21 euro/km
+
+*/
